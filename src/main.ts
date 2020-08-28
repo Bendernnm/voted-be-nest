1cbb423b850b2a2ef8e4ec3650c15d45cd26f736
bender <bender.d1mon@gmail.com>
2020-08-28 19:05:09 +0300

    use global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-require('./source-maps');
-
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app/app.module';
 
+require('./source-maps');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+  }));
+
   await app.listen(3000);
 }
 
